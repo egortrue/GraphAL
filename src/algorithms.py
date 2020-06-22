717f8607851b2225f9790d8f5d5f84700fe05772
Andrey <bakurskii2001@gmail.com>
2020-06-22 10:32:37 +0300

    add function of checking errors, create interface log and definition of algorithm

--- src/algorithms.py
+++ src/algorithms.py
@@ -8,11 +8,11 @@ import time
 
 from graphs import *
 
+
 #########################################################################
 # define BFS function
 
 def BFS(app, start):
-
     alg_dll.BFS.argtypes = [ct.POINTER(Graph), ct.POINTER(Node)]
     alg_dll.BFS.restype = ct.POINTER(ct.POINTER(Node))
 
@@ -25,16 +25,17 @@ def BFS(app, start):
         print(i.contents.C_ID, end=" ")
     print("\n")
 
-    for node_ptr in path:
-
+    for ind, node_ptr in enumerate(path):
         c_node_id = node_ptr.contents.C_ID
         node = app.graph.get_node(c_node_id)
         node.color = "cyan"
         app.graph.update_nodes_colors()
 
+        app.write_to_log_out(ind)
+
         app.draw_graph()
         time.sleep(0.5)
-        
+
 
 #########################################################################
 # define DFS function
@@ -53,24 +54,23 @@ def DFS(app, start):
         print(i.contents.C_ID, end=" ")
     print("\n")
 
-    for node_ptr in path:
-
+    for ind, node_ptr in enumerate(path):
         c_node_id = node_ptr.contents.C_ID
         node = app.graph.get_node(c_node_id)
         node.color = "cyan"
         app.graph.update_nodes_colors()
 
+        app.write_to_log_out(ind + 1)
         app.draw_graph()
         time.sleep(0.5)
-        
+
 
 #########################################################################
 # define Dijkstra function
 
 def Dijkstra(app, start):
-
     alg_dll.Dijkstra.argtypes = [ct.POINTER(Graph), ct.POINTER(Node)]
-    alg_dll.Dijkstra.restype  = ct.POINTER(ct.POINTER(Node))
+    alg_dll.Dijkstra.restype = ct.POINTER(ct.POINTER(Node))
 
     path = alg_dll.Dijkstra(app.graph.ptr, start.ptr)
     path = ct.cast(path, ct.POINTER(ct.POINTER(Node) * app.graph.nodes_num)).contents
@@ -81,15 +81,17 @@ def Dijkstra(app, start):
         print(i.contents.C_ID, end=" ")
     print("\n")
 
-    for node_ptr in path:
-
+    for ind, node_ptr in enumerate(path):
         c_node_id = node_ptr.contents.C_ID
         node = app.graph.get_node(c_node_id)
         node.color = "cyan"
         app.graph.update_nodes_colors()
 
+        app.write_to_log_out(ind + 1)
+
         app.draw_graph()
         time.sleep(0.25)
+
 
 
 
@@ -97,24 +99,21 @@ def Dijkstra(app, start):
 # define Prim function
 
 def Prim(app, root):
-
     if app.graph.directed:
         return 0
 
     alg_dll.Prim.argtypes = [ct.POINTER(Graph), ct.POINTER(Node)]
-    alg_dll.Prim.restype  = ct.POINTER(ct.POINTER(Edge))
+    alg_dll.Prim.restype = ct.POINTER(ct.POINTER(Edge))
 
     path = alg_dll.Prim(app.graph.ptr, root.ptr)
-    path = ct.cast(path, ct.POINTER(ct.POINTER(Edge) * (app.graph.nodes_num-1))).contents
+    path = ct.cast(path, ct.POINTER(ct.POINTER(Edge) * (app.graph.nodes_num - 1))).contents
 
     # the check
     print("Prim")
     for i in path:
         print(i.contents.C_SOURCE.contents.C_ID, i.contents.C_TARGET.contents.C_ID)
 
-
-    for edge_ptr in path:
-
+    for ind, edge_ptr in enumerate(path):
         c_source_id = edge_ptr.contents.C_SOURCE.contents.C_ID
         c_target_id = edge_ptr.contents.C_TARGET.contents.C_ID
 
@@ -122,52 +121,11 @@ def Prim(app, root):
         edge.color = "#FF0000"
         app.graph.update_edges_colors()
 
-        app.draw_graph()
-        time.sleep(0.5)
-
-    # Hide non-releavnt edges
-    for i in range(app.graph.edges_num):
-        if app.graph.edges_color[i] == graph.edges_default_color:
-            app.graph.edges_color[i] = "#FFFFFF"  
-
-    app.draw_graph()
-
-
-
-#########################################################################
-# define Prim function
-
-def Kruskal(app):
-
-    if graph.directed:
-        return 0
-
-
-    alg_dll.Kruskal.argtypes = [ct.POINTER(Graph)]
-    alg_dll.Kruskal.restype  = ct.POINTER(ct.POINTER(Edge))
-
-    path = alg_dll.Kruskal(app.graph.ptr)
-    path = ct.cast(path, ct.POINTER(ct.POINTER(Edge) * (app.graph.nodes_num-1))).contents
-
-    # the check
-    print("Kruskal")
-    for i in path:
-        print(i.contents.C_SOURCE.contents.C_ID, i.contents.C_TARGET.contents.C_ID)
-
-
-    for edge_ptr in path:
-
-        c_source_id = edge_ptr.contents.C_SOURCE.contents.C_ID
-        c_target_id = edge_ptr.contents.C_TARGET.contents.C_ID
-
-        edge = app.graph.get_edge(c_source_id, c_target_id)
-        edge.color = "#FF0000"
-        app.graph.update_edges_colors()
+        app.write_to_log_out(ind + 1, "edges")
 
         app.draw_graph()
         time.sleep(0.5)
 
-        
     # Hide non-releavnt edges
     for i in range(app.graph.edges_num):
         if app.graph.edges_color[i] == app.graph.edges_default_color:
@@ -175,5 +133,43 @@ def Kruskal(app):
 
     app.draw_graph()
 
+
+#########################################################################
+# define Prim function
+
+def Kruskal(app):
+    if app.graph.directed:
+        return 0
+
+    alg_dll.Kruskal.argtypes = [ct.POINTER(Graph)]
+    alg_dll.Kruskal.restype = ct.POINTER(ct.POINTER(Edge))
+
+    path = alg_dll.Kruskal(app.graph.ptr)
+    path = ct.cast(path, ct.POINTER(ct.POINTER(Edge) * (app.graph.nodes_num - 1))).contents
+
+    # the check
+    print("Kruskal")
+    for i in path:
+        print(i.contents.C_SOURCE.contents.C_ID, i.contents.C_TARGET.contents.C_ID)
+
+    for ind, edge_ptr in enumerate(path):
+        c_source_id = edge_ptr.contents.C_SOURCE.contents.C_ID
+        c_target_id = edge_ptr.contents.C_TARGET.contents.C_ID
+
+        edge = app.graph.get_edge(c_source_id, c_target_id)
+        edge.color = "#FF0000"
+        app.graph.update_edges_colors()
+
+        app.write_to_log_out(ind + 1, "edges")
+
+        app.draw_graph()
+        time.sleep(0.5)
+
+    # Hide non-releavnt edges
+    for i in range(app.graph.edges_num):
+        if app.graph.edges_color[i] == app.graph.edges_default_color:
+            app.graph.edges_color[i] = "#FFFFFF"
+
+    app.draw_graph()
 
 #########################################################################
